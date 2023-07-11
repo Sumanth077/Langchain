d6d648d2fc3ce38ac261bbab8692e114a74915f5
Sumanth <66694715+Sumanth077@users.noreply.github.com>
2023-07-12 01:00:46 +0530

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,6 +1,9 @@
 import streamlit as st
+from langchain.llms import OpenAI
+
 from clarifai.auth.helper import ClarifaiAuthHelper
 from clarifai.client import create_stub
+from clarifai_utils.modules.css import ClarifaiStreamlitCSS
 from clarifai.listing.lister import ClarifaiResourceLister
 from clarifai.modules.css import ClarifaiStreamlitCSS
 from google.protobuf import json_format, timestamp_pb2
@@ -13,37 +16,28 @@ auth = ClarifaiAuthHelper.from_streamlit(st)
 stub = create_stub(auth)
 userDataObject = auth.get_user_app_id_proto()
 lister = ClarifaiResourceLister(stub, auth.user_id, auth.app_id, page_size=16)
-st.title("Simple example to list inputs")
 
-with st.form(key="data-inputs"):
-  mtotal = st.number_input(
-      "Select number of inputs to view in a table:", min_value=10, max_value=100)
-  submitted = st.form_submit_button('Submit')
 
-if submitted:
-  if mtotal is None or mtotal == 0:
-    st.warning("Number of inputs must be provided.")
-    st.stop()
-  else:
-    st.write("Number of inputs in table will be: {}".format(mtotal))
+st.set_page_config(layout="wide")
 
-  # Stream inputs from the app
-  all_inputs = []
-  for inp in lister.inputs_generator():
-    all_inputs.append(inp)
+ClarifaiStreamlitCSS.insert_default_css(st)
 
-    if len(all_inputs) >= mtotal:
-      break
+st.title("Simple App to get started with Langchain")
 
-  # Get some common stuff out of the inputs.
-  data = []
-  for inp in all_inputs:
-    data.append({
-        "id": inp.id,
-        "status": inp.status.description,
-        "created_at": timestamp_pb2.Timestamp.ToDatetime(inp.created_at),
-        "modified_at": timestamp_pb2.Timestamp.ToDatetime(inp.modified_at),
-        "metadata": json_format.MessageToDict(inp.data.metadata),
-    })
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
 
-  st.dataframe(data)
+
+def generate(input_text):
+    llm = OpenAI(temperature=0.8, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+
+with st.form("my_form"):
+    text = st.text_area("Enter text:", "Suggest a Roadmap to learn Machine Learning?")
+    submitted = st.form_submit_button("Submit")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+    elif submitted:
+        generate(text)
+
