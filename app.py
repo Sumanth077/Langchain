e0dc2678242082ada2bdc1d8f2e277a09e1c06ee
Sumanth <66694715+Sumanth077@users.noreply.github.com>
2023-07-12 01:01:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
+import os
+
 import streamlit as st
-from langchain.llms import OpenAI
 
-from clarifai_utils.modules.css import ClarifaiStreamlitCSS
-
-st.set_page_config(layout="wide")
-
-ClarifaiStreamlitCSS.insert_default_css(st)
-
-# st.markdown("Please select a specific page from the sidebar to the left")
-
-st.title("Simple App to get started with Langchain")
-
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", type="password")
-
-
-def generate(input_text):
-    llm = OpenAI(temperature=0.8, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
-
-
-with st.form("my_form"):
-    text = st.text_area("Enter text:", "Suggest a Roadmap to learn Machine Learning?")
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-    elif submitted:
-        generate(text)
+st.set_page_config(layout="wide", page_icon="https://clarifai.com/favicon.svg")
