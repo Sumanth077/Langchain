f2734aa404fa3cc7831f2e0f1f6d221dee04f9e3
Sumanth <66694715+Sumanth077@users.noreply.github.com>
2023-07-12 13:16:25 +0530

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -17,19 +17,24 @@ stub = create_stub(auth)
 userDataObject = auth.get_user_app_id_proto()
 lister = ClarifaiResourceLister(stub, auth.user_id, auth.app_id, page_size=16)
 
-st.title("Simple App to get started with Langchain")
+st.title("🦜 Simple Application to get started with Langchain")
+
+st.sidebar.markdown("Made with UI Modules, a streamlit integration from [Clarifai](https://clarifai.com/) that helps you create and deploy beautiful AI web apps.")
+st.sidebar.image(
+    "https://rasahq.github.io/rasa-nlu-examples/square-logo.svg", width=100
+)
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", type="password")
 
 
-def generate(input_text):
+def generate(text):
     llm = OpenAI(temperature=0.8, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    st.info(llm(text))
 
 
 with st.form("my_form"):
-    text = st.text_area("Enter text:", "Suggest a Roadmap to learn Machine Learning?")
+    text = st.text_area("Enter text:", "What are the potential risks and concerns surrounding AI development?")
     submitted = st.form_submit_button("Submit")
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
